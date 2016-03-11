23dd9837d890cc31c667c1a6183c828a9a491410
mtokle <maheshtokle@gmail.com>
2016-03-11 12:24:34 +0530

    Local actor enabled

--- Tavisca.USG.TestSuit/HotelSearchProviderFixture.cs
+++ Tavisca.USG.TestSuit/HotelSearchProviderFixture.cs
@@ -30,8 +30,8 @@ namespace Tavisca.USG.TestSuit
             IResultStoreManager _resultStoreManager = new MockResultStoreManager();
             _searchProvider = new HotelSearchProvider(new MockSessionStateManager(), _resultStoreManager, new AkkaTaskManager());
             SystemActors.HotelSearchActor = ActorSystem.ActorOf(Props.Create(() => new HotelSearchActor(_configManager, _metadataManager, _contentManager, _connectorFactory, _resultStoreManager)));
-            //SystemActors.SearchBroadcastActor = ActorSystem.ActorOf(Props.Create(() => new SearchBroadcastActor(new MockHotelConnectorFactory())), "broadcaster"); //local connector actor
-            SystemActors.SearchBroadcastActor = ActorSystem.ActorOf(Props.Empty.WithRouter(FromConfig.Instance), "supplier");
+            SystemActors.SearchBroadcastActor = ActorSystem.ActorOf(Props.Create(() => new SearchBroadcastActor(new MockHotelConnectorFactory())), "broadcaster"); //local connector actor
+            //SystemActors.SearchBroadcastActor = ActorSystem.ActorOf(Props.Empty.WithRouter(FromConfig.Instance), "supplier");
         }
 
         [TestMethod]
